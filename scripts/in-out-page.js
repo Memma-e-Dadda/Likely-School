de1515988a085f70366874c99ac17bddb53204cb
iTzDavid07 <david.chrostek07@gmail.com>
2023-05-24 03:50:43 +0200

    feat: Responsive Mobile finished
    manca solo il responsive della pagina Team.html

--- scripts/in-out-page.js
+++ scripts/in-out-page.js
@@ -3,33 +3,35 @@ var threshold;
 
 const site = window.location.pathname;
 
-if(site == "/"){
+if (site == "/") {
     threshold = 0.6;
-} else if(site == "/Team.html"){
+} else if (site == "/Team.html") {
     threshold = 0.55;
 }
 
-var callback = function(items){
+var callback = function (items) {
     items.forEach((item) => {
-        if(item.isIntersecting){
+        if (item.isIntersecting) {
             item.target.classList.add("in-page");
-        } else{
+        } else {
             item.target.classList.remove("in-page");
         }
     });
 
     elements_to_watch.forEach((element) => {
 
-        if(element.offsetTop + window.innerHeight * (1 - threshold) <= window.pageYOffset){
+        if (element.offsetTop + window.innerHeight * (1 - threshold) <= window.pageYOffset) {
             element.classList.add("out-page");
-        } else{
+        } else {
             element.classList.remove("out-page");
         }
     });
 }
 
-var observer = new IntersectionObserver(callback, { threshold: threshold } );
+var observer = new IntersectionObserver(callback, { threshold: threshold });
 
-elements_to_watch.forEach((element) => {
-    observer.observe(element);
-});
+if (window.innerWidth > 390) {
+    elements_to_watch.forEach((element) => {
+        observer.observe(element);
+    });
+}
